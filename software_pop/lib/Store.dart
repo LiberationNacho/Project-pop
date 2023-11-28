561ae90fc36041bf3e362b48cac1ab8d008022a8
wpqlks7 <wpqlks7@gmail.com>
2023-11-28 16:15:13 +0900

    클래스생성

---
+++ software_pop/lib/Store.dart
@@ -0,0 +1,20 @@
+class Store
+{
+  int _StoreName = 0; //0 = 옷, 1 = 가구, 2 = 음악 상점
+  int _SelectItem = 0; //다양한 아이템 중 어떤 아이템을 선택했는지 체크하는 변수
+
+  void ChooseStoreName(int num)
+  {
+    this._StoreName = num;
+  }
+
+  void ChooseItem(int num)
+  {
+    this._SelectItem = num;
+  }
+}
+
+class StoreUI
+{
+  
+}
