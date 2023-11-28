a6483d9cab18ac00eaf95f10c9d93f099a4b0e50
man25816 <man25816@naver.com>
2023-11-28 15:07:29 +0900

    Animal.dart 수정 1차

--- software_pop/lib/Animal.dart
+++ software_pop/lib/Animal.dart
@@ -1,3 +1,5 @@
+// import 'package:flutter/material.dart';
+
 class Animal {
   String _name = "";
   int _hunger = 0; // 허기진 정도를 나타내는 변수
@@ -66,3 +68,127 @@ class AnimalUI {
     }
   }
 }
+
+/*
+void main() {
+  runApp(const MyApp());
+}
+
+// 동물을 나타내는 클래스
+class Animal {
+  String _name = "Buddy";
+  int _hunger = 0;
+
+  // 동물의 이름을 설정하는 메서드
+  void setName(String name) {
+    _name = name;
+  }
+
+  // 동물이 먹는 메서드
+  void eat() {
+    _hunger = 0;
+    print("$_name is eating. Hunger level: $_hunger");
+  }
+
+  // 동물이 자는 메서드
+  void sleep() {
+    print("$_name is sleeping.");
+  }
+
+  // 동물이 놀기 메서드
+  void play() {
+    print("$_name is playing.");
+  }
+
+  // 동물의 정보를 출력하는 메서드
+  void displayInfo() {
+    print("Name: $_name, Hunger: $_hunger");
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pet Simulator',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(title: 'Pet Simulator Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Animal myPet = Animal();
+
+  // 동물과 상호 작용하는 메서드
+  void _interactWithPet() {
+    myPet.displayInfo();
+  }
+
+  // 동물에게 먹이 주기
+  void _feedPet() {
+    myPet.eat();
+    myPet.displayInfo();
+  }
+
+  // 동물을 재우기
+  void _putPetToSleep() {
+    myPet.sleep();
+    myPet.displayInfo();
+  }
+
+  // 동물과 놀기
+  void _playWithPet() {
+    myPet.play();
+    myPet.displayInfo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Click the buttons to interact with your pet:',
+            ),
+            ElevatedButton(
+              onPressed: _interactWithPet,
+              child: const Text('Interact'),
+            ),
+            ElevatedButton(
+              onPressed: _feedPet,
+              child: const Text('Feed'),
+            ),
+            ElevatedButton(
+              onPressed: _putPetToSleep,
+              child: const Text('Sleep'),
+            ),
+            ElevatedButton(
+              onPressed: _playWithPet,
+              child: const Text('Play'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+ */
