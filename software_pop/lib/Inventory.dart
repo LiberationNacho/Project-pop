c5f61d2d92cdd4df0c51dd14a86066114115fffc
wpqlks7 <wpqlks7@gmail.com>
2023-11-28 16:36:57 +0900

    기본 UI 제작

--- software_pop/lib/Inventory.dart
+++ software_pop/lib/Inventory.dart
@@ -1,20 +1,75 @@
-class Inventory
-{
-  int _StoreName = 0; //0 = 옷, 1 = 가구, 2 = 음악 상점
-  int _SelectItem = 0; //다양한 아이템 중 어떤 아이템을 선택했는지 체크하는 변수
+import 'package:flutter/material.dart';
 
-  void ChooseStoreName(int num)
-  {
-    this._StoreName = num;
+class Inventory {
+  int _storeName = 0; // 0 = 옷, 1 = 가구, 2 = 음악 상점
+  int _selectItem = 0; // 다양한 아이템 중 어떤 아이템을 선택했는지 체크하는 변수
+
+  void chooseStoreName(int num) {
+    _storeName = num;
   }
 
-  void ChooseItem(int num)
-  {
-    this._SelectItem = num;
+  void chooseItem(int num) {
+    _selectItem = num;
   }
 }
 
-class InventoryUI
-{
-  
+class InventoryUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Inventory UI'),
+        ),
+        body: InventoryGrid(),
+      ),
+    );
+  }
+}
+
+class InventoryGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5, // 열의 개수
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        // 여기서 각 버튼을 만들어서 반환
+        return InventoryButton(index);
+      },
+      itemCount: 15, // 총 버튼의 개수 (3x5)
+    );
+  }
+}
+
+class InventoryButton extends StatelessWidget {
+  final int index;
+
+  InventoryButton(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // 버튼이 눌렸을 때 실행되는 코드
+        print('Button $index pressed.');
+        // 여기서 필요한 로직을 추가하세요.
+      },
+      child: Container(
+        margin: EdgeInsets.all(8.0),
+        color: Colors.blue, // 버튼 색상
+        child: Center(
+          child: Text(
+            'Item $index',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(InventoryUI());
 }
--- software_pop/lib/Store.dart
+++ software_pop/lib/Store.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Store
 {
   int _StoreName = 0; //0 = 옷, 1 = 가구, 2 = 음악 상점
@@ -14,7 +16,63 @@ class Store
   }
 }
 
-class StoreUI
-{
-  
+class StoreUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Store UI'),
+        ),
+        body: StoreGrid(),
+      ),
+    );
+  }
+}
+
+class StoreGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5, // 열의 개수
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        // 여기서 각 버튼을 만들어서 반환
+        return StoreButton(index);
+      },
+      itemCount: 15, // 총 버튼의 개수 (3x5)
+    );
+  }
+}
+
+class StoreButton extends StatelessWidget {
+  final int index;
+
+  StoreButton(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // 버튼이 눌렸을 때 실행되는 코드
+        print('Button $index pressed.');
+        // 여기서 필요한 로직을 추가하세요.
+      },
+      child: Container(
+        margin: EdgeInsets.all(8.0),
+        color: Colors.blue, // 버튼 색상
+        child: Center(
+          child: Text(
+            'Item $index',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(StoreUI());
 }
