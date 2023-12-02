1f06747ee1354e3c96e9f7edf9a6b2186cc09980
man25816 <man25816@naver.com>
2023-12-02 16:55:04 +0900

    친밀도, 공복지수, 상태 추가

--- software_pop/lib/Animal.dart
+++ software_pop/lib/Animal.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
 }
 
+enum CatState {
+  Awake,
+  Sleeping,
+}
+
 class Animal {
   String petStatus = ''; // 동물 상태를 저장하는 변수
+  int hungry = 100; // 공복지수
+  int affection = 0; // 친밀도
+  CatState catState = CatState.Awake;
 
   void displayInfo() {
     petStatus = 'Animal info';
@@ -55,11 +64,49 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Animal myPet = Animal();
   String currentImage = 'images/popcat.jpg';
+  Timer? hungerTimer;
+  int interactionCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 1분마다 공복지수를 1씩 감소시키는 타이머 설정
+    hungerTimer = Timer.periodic(Duration(minutes: 1), (Timer timer) {
+      setState(() {
+        myPet.hungry -= 1;
+        if (myPet.hungry < 0) {
+          myPet.hungry = 0;
+        }
+      });
+    });
+
+    // 밤 10시부터 아침 6시까지 자는 상태로 전환
+    Timer.periodic(Duration(minutes: 1), (Timer timer) {
+      var now = DateTime.now();
+      if (now.hour >= 22 || now.hour < 6) {
+        setState(() {
+          myPet.catState = CatState.Sleeping;
+        });
+      } else {
+        setState(() {
+          myPet.catState = CatState.Awake;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    hungerTimer?.cancel(); // 페이지가 dispose될 때 타이머 해제
+    super.dispose();
+  }
 
   // 동물과 상호 작용하는 메서드
   void _interactWithPet() {
     setState(() {
       myPet.displayInfo();
+      _increaseAffection(1);
     });
   }
 
@@ -67,13 +114,11 @@ class _MyHomePageState extends State<MyHomePage> {
   void _feedPet() {
     setState(() {
       myPet.eat();
-    });
-  }
-
-  // 동물을 재우기
-  void _putPetToSleep() {
-    setState(() {
-      myPet.sleep();
+      myPet.hungry += 10;
+      if (myPet.hungry > 100) {
+        myPet.hungry = 100;
+      }
+      _increaseAffection(1);
     });
   }
 
@@ -81,6 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _playWithPet() {
     setState(() {
       myPet.play();
+      _increaseAffection(1);
     });
   }
 
@@ -91,6 +137,16 @@ class _MyHomePageState extends State<MyHomePage> {
           ? 'images/popcat2.jpg'
           : 'images/popcat.jpg';
     });
+    _increaseAffection(1);
+  }
+
+  // 친밀도 증가 메서드, 버튼 클릭의 합이 50이상이면 친밀도 1증가
+  void _increaseAffection(int value) {
+    interactionCount += value;
+    if (interactionCount >= 50) {
+      myPet.affection += 1;
+      interactionCount = 0;
+    }
   }
 
   @override
@@ -106,7 +162,13 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'Click the buttons to interact with your pet:',
             ),
-            SizedBox(height: 20), // 버튼 위 간격
+            SizedBox(height: 10), // 버튼 위 간격
+            // 추가: 동물 상태와 공복지수, 친밀도를 표시하는 Text 위젯
+            Text(myPet.petStatus),
+            Text('Hungry: ${myPet.hungry}'),
+            Text('Affection: ${myPet.affection}'),
+            Text('Cat State: ${myPet.catState}'),
+            SizedBox(height: 20), // 이미지와 버튼 간 간격
             // GestureDetector를 사용하여 이미지 클릭 감지
             GestureDetector(
               onTap: _changeImage,
@@ -117,28 +179,32 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             SizedBox(height: 20), // 이미지와 버튼 간 간격
-            ElevatedButton(
-              onPressed: _interactWithPet,
-              child: const Text('상호작용'),
+            // 밤 10시부터 아침 6시까지 잠자기 상태인 경우 "동물 잠잠" 메시지 표시
+            if (myPet.catState == CatState.Sleeping)
+              Text(
+                '동물 잠잠',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            // 가로로 정렬된 버튼들
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _interactWithPet,
+                  child: const Text('상호작용'),
+                ),
+                SizedBox(width: 10), // 버튼 간격 조절
+                ElevatedButton(
+                  onPressed: _feedPet,
+                  child: const Text('먹이 주기'),
+                ),
+                SizedBox(width: 10), // 버튼 간격 조절
+                ElevatedButton(
+                  onPressed: _playWithPet,
+                  child: const Text('놀아주기'),
+                ),
+              ],
             ),
-            SizedBox(height: 10), // 버튼 간격 조절
-            ElevatedButton(
-              onPressed: _feedPet,
-              child: const Text('먹이 주기'),
-            ),
-            SizedBox(height: 10), // 버튼 간격 조절
-            ElevatedButton(
-              onPressed: _putPetToSleep,
-              child: const Text('재우기'),
-            ),
-            SizedBox(height: 10), // 버튼 간격 조절
-            ElevatedButton(
-              onPressed: _playWithPet,
-              child: const Text('놀아주기'),
-            ),
-            SizedBox(height: 20), // 버튼 아래 간격
-            // 추가: 동물 상태를 표시하는 Text 위젯
-            Text(myPet.petStatus),
           ],
         ),
       ),
